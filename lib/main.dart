10b07ebe285e94a789bef73978d730189930f092
moawees <moawees@gmail.com>
2025-05-18 19:56:54 +0300

    connect app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/app/foodie_app.dart';
 import 'package:foodie/core/router/app_router.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 void main()async {
- 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( FoodieApp(appRouter: AppRouter(),));
 }
 
